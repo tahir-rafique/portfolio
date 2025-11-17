d88f172743c00cb5818971adc8e5582296fe650d
itstahirrafique <121024103+tahir-rafique@users.noreply.github.com>
2025-11-17 21:38:01 +0500

    latest code

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>
-      <h1>hello jee</h1>
+    <div className='min-h-screen min-w-screen bg-violet-400 flex justify-center items-center'>
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-black text-center font-stretch-expanded">Coming Soon</h1>
     </div>
   )
 }
